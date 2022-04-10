5ef94a8aebd2abe9559b8ae682884b75956417c5
Liam Martin <liam.martin@anpriot.com>
2022-04-10 22:22:32 +0100

    Set up project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Birthday Message Sender
+      </header>
+      <body className="App-body">
+        Body here
+      </body>
+    </div>
+  );
+}
+
+export default App;
